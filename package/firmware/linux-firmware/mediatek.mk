lsgxteam/libwrt-mod

linux-firmware: update to 20230515
Package offloading firmware for MT7981 which has been added to
linux-firmware recently and became available in this release.

Changes since 20230515:
51290942 Merge branch 'main' of https://github.com/CirrusLogic/linux-firmware
cc628d65 cirrus: Add firmware and tuning files for HP G10 series laptops
905d3265 linux-firmware: update firmware for mediatek bluetooth chip (MT7922)
d1962891 WHENCE: Cleanup Realtek BT firmware provenance
6569484e linux-firmware: update firmware for MT7922 WiFi device
7d639e80 cnm: update chips&media wave521c firmware.
b8a56bf2 cirrus: Add firmware and tuning files for Lenovo ThinkPad P1 Gen 6
6c9e0ed5 check_whence: error on directory listed as File
05183b7b check_whence: error on duplicate file entries
c4423c91 WHENCE: comment out duplicate MediaTek firmware
2bc50f50 Merge branch 'mtl_guc_70.6.6' of git://anongit.freedesktop.org/drm/drm-firmware
192ee6d1 i915: Add GuC v70.6.6 for MTL
312c61f5 amdgpu: update DCN 3.1.6 DMCUB firmware
0061a2dd rtl_bt: Update RTL8852B BT USB firmware to 0xDBC6_B20F
1de22a39 rtl_bt: Update RTL8761B BT USB firmware to 0xDFC6_D922
dee0d4cd rtl_bt: Update RTL8761B BT UART firmware to 0x9DC6_D922
fab14965 Group all Conexant V4L devices together
e88bdbe4 rtl_nic: update firmware of USB devices
6536a964 linux-firmware: Update firmware file for Intel Bluetooth AX200
2ca17876 linux-firmware: Update firmware file for Intel Bluetooth AX201
7610656f linux-firmware: Update firmware file for Intel Bluetooth AX203
209ba083 linux-firmware: Update firmware file for Intel Bluetooth AX203
a2739f05 linux-firmware: Update firmware file for Intel Bluetooth AX211
1ee587d5 linux-firmware: Update firmware file for Intel Bluetooth AX211
40ba7eee linux-firmware: Update firmware file for Intel Bluetooth AX210
bcbbf6bf linux-firmware: update firmware for MT7981
507ee862 Merge branch 'main' of github.com:sampathnimmmala/bt_firmware
2c6be1a4 qca: Update firmware files for BT chip WCN6750
8451c2b1 mt76xx: M

package/firmware/linux-firmware/mediatek.mk
@@ -69,6 +69,15 @@ define Package/mt7922bt-firmware/install
 endef
 $(eval $(call BuildPackage,mt7922bt-firmware))
 
+Package/mt7981-wo-firmware = $(call Package/firmware-default,MT7981 offload firmware)
+define Package/mt7981-wo-firmware/install
+	$(INSTALL_DIR) $(1)/lib/firmware/mediatek
+	$(INSTALL_DATA) \
+		$(PKG_BUILD_DIR)/mediatek/mt7981_wo.bin \
+		$(1)/lib/firmware/mediatek
+endef
+$(eval $(call BuildPackage,mt7981-wo-firmware))
+
 Package/mt7986-wo-firmware = $(call Package/firmware-default,MT7986 offload firmware)
 define Package/mt7986-wo-firmware/install
 	$(INSTALL_DIR) $(1)/lib/firmware/mediatek

mgrubertirol/friendlywrt

Signed-off-by: Daniel Golle <daniel@makrotopia.org>
(cherry-picked from commit d53fe5d9ce62c0fe9f5bc5c35cab15327a105fbd)
Signed-off-by: jensen <jensenhuang@friendlyarm.com>

package/firmware/linux-firmware/mediatek.mk
@@ -1,20 +1,20 @@
 Package/mt7601u-firmware = $(call Package/firmware-default,MediaTek MT7601U firmware)
 define Package/mt7601u-firmware/install
-	$(INSTALL_DIR) $(1)/lib/firmware
+	$(INSTALL_DIR) $(1)/lib/firmware/mediatek
 	$(INSTALL_DATA) \
-		$(PKG_BUILD_DIR)/mt7601u.bin \
-		$(1)/lib/firmware
+		$(PKG_BUILD_DIR)/mediatek/mt7601u.bin \
+		$(1)/lib/firmware/mediatek
 endef
 $(eval $(call BuildPackage,mt7601u-firmware))
 
 Package/mt76x2-firmware = $(call Package/firmware-default,MediaTek MT76x2 firmware, \
 	@!PACKAGE_kmod-mt76x2-common)
 define Package/mt76x2-firmware/install
-	$(INSTALL_DIR) $(1)/lib/firmware
+	$(INSTALL_DIR) $(1)/lib/firmware/mediatek
 	$(INSTALL_DATA) \
-		$(PKG_BUILD_DIR)/mt7662.bin \
-		$(PKG_BUILD_DIR)/mt7662_rom_patch.bin \
-		$(1)/lib/firmware
+		$(PKG_BUILD_DIR)/mediatek/mt7662.bin \
+		$(PKG_BUILD_DIR)/mediatek/mt7662_rom_patch.bin \
+		$(1)/lib/firmware/mediatek
 endef
 $(eval $(call BuildPackage,mt76x2-firmware))
 