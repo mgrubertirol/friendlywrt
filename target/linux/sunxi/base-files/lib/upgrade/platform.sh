cffd6ec92e4f021fc154db826ccf6f387538711d
Manfred Gruber <m.gruber@opendev.at>
2024-08-05 10:07:22 +0200

    changed platform.sh to use get_image_dd

--- target/linux/sunxi/base-files/lib/upgrade/platform.sh
+++ target/linux/sunxi/base-files/lib/upgrade/platform.sh
@@ -9,7 +9,7 @@ platform_check_image() {
 	get_partitions "/dev/$diskdev" bootdisk
 
 	#extract the boot sector from the image
-	get_image "$@" | dd of=/tmp/image.bs count=1 bs=512b 2>/dev/null
+	get_image_dd "$@" | dd of=/tmp/image.bs count=1 bs=512b 2>/dev/null
 
 	get_partitions /tmp/image.bs image
 
@@ -49,7 +49,7 @@ platform_do_upgrade() {
 		get_partitions "/dev/$diskdev" bootdisk
 
 		#extract the boot sector from the image
-		get_image "$@" | dd of=/tmp/image.bs count=1 bs=512b
+		get_image_dd "$@" | dd of=/tmp/image.bs count=1 bs=512b
 
 		get_partitions /tmp/image.bs image
 
@@ -60,7 +60,7 @@ platform_do_upgrade() {
 	fi
 
 	if [ -n "$diff" ]; then
-		get_image "$@" | dd of="/dev/$diskdev" bs=4096 conv=fsync
+		get_image_dd "$@" | dd of="/dev/$diskdev" bs=4096 conv=fsync
 
 		# Separate removal and addtion is necessary; otherwise, partition 1
 		# will be missing if it overlaps with the old partition 2
@@ -71,12 +71,12 @@ platform_do_upgrade() {
 	fi
 
 	#write uboot image
-	get_image "$@" | dd of="$diskdev" bs=1024 skip=8 seek=8 count=1016 conv=fsync
+	get_image_dd "$@" | dd of="$diskdev" bs=1024 skip=8 seek=8 count=1016 conv=fsync
 	#iterate over each partition from the image and write it to the boot disk
 	while read part start size; do
 		if export_partdevice partdev $part; then
 			echo "Writing image to /dev/$partdev..."
-			get_image "$@" | dd of="/dev/$partdev" ibs="512" obs=1M skip="$start" count="$size" conv=fsync
+			get_image_dd "$@" | dd of="/dev/$partdev" ibs="512" obs=1M skip="$start" count="$size" conv=fsync
 		else
 			echo "Unable to find partition $part device, skipped."
 		fi
@@ -84,5 +84,5 @@ platform_do_upgrade() {
 
 	#copy partition uuid
 	echo "Writing new UUID to /dev/$diskdev..."
-	get_image "$@" | dd of="/dev/$diskdev" bs=1 skip=440 count=4 seek=440 conv=fsync
+	get_image_dd "$@" | dd of="/dev/$diskdev" bs=1 skip=440 count=4 seek=440 conv=fsync
 }
